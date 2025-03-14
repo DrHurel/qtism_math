805a3e69ef5cf5fbf5eb46c487af2598f150b3de
Geoffreypierre <geoffrey.pierre@etu.umontpellier.fr>
2025-03-14 13:10:39 +0100

    setup : (add speech text + chat gpt sdk + home page)

---
+++ lib/services/speech_text.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+
+class SpeechText {
+  static Future<bool> toggleRecording(
+      Function(String message) message, ValueChanged<bool> onListening) async {
+    stt.SpeechToText speech = stt.SpeechToText();
+    final bool available = await speech.initialize(
+        onStatus: (value) => onListening(speech.isListening), onError: print);
+    if (speech.isListening) {
+      speech.stop();
+      return true;
+    }
+    if (available) {
+      speech.listen(onResult: (value) => message(value.recognizedWords));
+    } else {
+      print("The user has denied the use of speech recognition.");
+    }
+    return available;
+  }
+}
