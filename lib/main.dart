c3e5f22c1db43e625586dac4db11922a367bcec8
Hurel <jhurel@free-mobile.fr>
2025-04-10 14:36:29 +0200

    add: initialize Firebase in main application entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:qtism_math/pages/home.dart';
-//import 'package:qtism_math/pages/qt.dart';
-//import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
+
 
 void main() async {
-  //await dotenv.load(fileName: ".env");
+
+await Firebase.initializeApp(
+
+    options: DefaultFirebaseOptions.currentPlatform,
+
+);
   runApp(const MyApp());
 }
 
