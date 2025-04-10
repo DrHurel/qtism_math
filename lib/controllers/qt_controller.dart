e958f20271d67b4493affee120fa448147289c79
Hurel <jhurel@free-mobile.fr>
2025-04-10 15:08:16 +0200

    add: refactor QTController and introduce EmotionService, InputService, and ProblemService for improved state management and problem generation

--- lib/controllers/qt_controller.dart
+++ lib/controllers/qt_controller.dart
@@ -1,153 +1,138 @@
 import 'package:flutter/material.dart';
 import 'package:qtism_math/common/app_strings.dart';
 import 'package:qtism_math/common/enum.dart';
-import 'package:qtism_math/services/problem_generator.dart';
-import 'package:qtism_math/services/emotion_manager.dart';
-import 'package:qtism_math/services/speech_text.dart';
-import 'package:qtism_math/services/math_expression_processor.dart';
 import 'package:qtism_math/services/text_to_speech_service.dart';
-import 'package:qtism_math/services/emotion_detector.dart';
-import 'package:qtism_math/services/voice_input_parser.dart';
-import 'package:qtism_math/utils/string_formatter.dart';
+import 'package:qtism_math/services/emotion_service.dart';
+import 'package:qtism_math/services/problem_service.dart';
+import 'package:qtism_math/services/input_service.dart';
 
 class QTController {
   // State variables
-  String transcribedText = "";
   String resultText = "";
-  bool isListening = false;
+  bool isCorrectAnswer = false;
+  bool isDefaultBubbleStyle = true;
   String currentEmotion = "neutral";
   String previousEmotion = "neutral";
   bool isTransitioning = false;
-  bool isCorrectAnswer = false;
-  bool isDefaultBubbleStyle = true;
-  ProblemType lastButtonClicked = ProblemType.none;
-  String generatedProblem = "";
-  bool? expectedTruthValue;
+  String transcribedText = "";
+  bool isListening = false;
 
   // Services
   final TextToSpeechService _ttsService = TextToSpeechService();
-  final EmotionManager emotionManager = EmotionManager();
+  late final EmotionService _emotionService;
+  late final ProblemService _problemService;
+  late final InputService _inputService;
 
   // State change notifier
   final Function(VoidCallback) setState;
 
-  QTController(this.setState);
-
-  // Forwards the calculation to the math service
-  int calculateCorrectAnswer(String operation) {
-    return MathExpressionProcessor.calculateResult(operation);
-  }
-
-  // Uses the TTS service to speak text
-  Future<void> speakResult(String text) async {
-    await _ttsService.speak(text);
-  }
-
-  void clearTranscription() {
-    setState(() {
-      transcribedText = "";
-    });
-  }
-
-  void generateCalculationProblem() {
-    final problemData = ProblemGenerator.generateCalculationProblem();
-    final problem = problemData['problem'];
-
-    _updateProblemState(
-      ProblemType.calculation,
-      problem,
-      StringFormatter.format(AppStrings.calculateResult, [problem]),
-      null // No truth value for calculation problems
-    );
-
-    speakResult(StringFormatter.format(AppStrings.calculateResultSpoken, [problem]));
-    resetEmotion();
-  }
-
-  void generateTrueOrFalseProblem() {
-    final problemData = ProblemGenerator.generateTrueOrFalseProblem();
-    final problem = problemData['problem'];
-    final isTrue = problemData['isTrue'];
-
-    _updateProblemState(
-      ProblemType.trueOrFalse,
-      problem,
-      StringFormatter.format(AppStrings.trueOrFalseQuestion, [problem]),
-      isTrue
-    );
-
-    speakResult(StringFormatter.format(AppStrings.trueOrFalseQuestion, [problem]));
-    resetEmotion();
+  QTController(this.setState) {
+    _initServices();
   }
   
-  // Helper method to update problem-related state
-  void _updateProblemState(ProblemType type, String problem, String displayText, bool? truthValue) {
+  void _initServices() {
+    _emotionService = EmotionService(
+      onEmotionChanged: _handleEmotionChanged,
+    );
+    
+    _problemService = ProblemService(
+      onProblemStateChanged: _handleProblemStateChanged,
+    );
+    
+    _inputService = InputService(
+      onTranscriptionChanged: _handleTranscriptionChanged,
+      onListeningStateChanged: _handleListeningStateChanged,
+    );
+  }
+  
+  // Callback handlers
+  void _handleEmotionChanged(String emotion, String previousEmotion, bool isTransitioning) {
     setState(() {
-      lastButtonClicked = type;
-      generatedProblem = problem;
+      this.currentEmotion = emotion;
+      this.previousEmotion = previousEmotion;
+      this.isTransitioning = isTransitioning;
+    });
+  }
+  
+  void _handleProblemStateChanged(String displayText, bool isDefaultBubble) {
+    setState(() {
       resultText = displayText;
-      expectedTruthValue = truthValue;
-      isDefaultBubbleStyle = true;
+      isDefaultBubbleStyle = isDefaultBubble;
     });
   }
-
-  void resetEmotion() {
-    if (emotionManager.emotion != "neutral") {
-      setState(() {
-        emotionManager.setEmotion("neutral");
-        isTransitioning = true;
-      });
-    }
-  }
-
-  Future<void> handleVoiceInput(AnimationController animationController) async {
-    resetEmotion();
-    _resetAnimation(animationController);
-
-    await SpeechText.toggleRecording(
-      _updateTranscription,
-      _processVoiceResult,
-      clearTranscription,
-      _updateListeningState,
-    );
-  }
   
-  // Helper methods to handle callbacks
-  void _updateTranscription(String transcription) {
+  void _handleTranscriptionChanged(String transcription) {
     setState(() {
       transcribedText = transcription;
     });
   }
   
-  void _processVoiceResult(String expression, String result) {
-    if (lastButtonClicked == ProblemType.calculation) {
-      handleCalculationVoiceSubmission(transcribedText);
-    } else if (lastButtonClicked == ProblemType.trueOrFalse) {
-      handleTrueOrFalseVoiceSubmission(transcribedText);
-    } else {
-      showResult(result, null);
-    }
+  void _handleListeningStateChanged(bool isListening) {
+    setState(() {
+      this.isListening = isListening;
+    });
   }
   
-  void _updateListeningState(bool value) {
-    setState(() {
-      isListening = value;
+  // Public API methods
+  Future<void> speakResult(String text) async {
+    await _ttsService.speak(text);
+  }
+  
+  void clearTranscription() {
+    _inputService.clearTranscription();
+  }
+  
+  void generateCalculationProblem() {
+    String textToSpeak = _problemService.generateCalculationProblem();
+    speakResult(textToSpeak);
+    _emotionService.resetEmotion();
+  }
+  
+  void generateTrueOrFalseProblem() {
+    String textToSpeak = _problemService.generateTrueOrFalseProblem();
+    speakResult(textToSpeak);
+    _emotionService.resetEmotion();
+  }
+  
+  void resetEmotion() {
+    _emotionService.resetEmotion();
+  }
+  
+  Future<void> handleVoiceInput(AnimationController animationController) async {
+    resetEmotion();
+    _resetAnimation(animationController);
+    
+    await _inputService.handleVoiceInput((result) {
+      _processVoiceResult(result, animationController);
     });
+  }
+  
+  void _processVoiceResult(String text, AnimationController? animationController) {
+    switch (_problemService.currentProblemType) {
+      case ProblemType.calculation:
+        handleCalculationVoiceSubmission(text, animationController);
+        break;
+      case ProblemType.trueOrFalse:
+        handleTrueOrFalseVoiceSubmission(text, animationController);
+        break;
+      default:
+        showResult(text, animationController);
+    }
   }
   
   void _resetAnimation(AnimationController animationController) {
     animationController.reset();
     animationController.forward();
   }
-
+  
   void handleSubmitted(String text, AnimationController animationController) {
     if (text.isEmpty) return;
-
+    
     setState(() {
       transcribedText = text;
     });
-
-    switch (lastButtonClicked) {
+    
+    switch (_problemService.currentProblemType) {
       case ProblemType.calculation:
         handleCalculationSubmission(text, animationController);
         break;
@@ -155,112 +140,47 @@ class QTController {
         handleTrueOrFalseSubmission(text, animationController);
         break;
       default:
-        _processGeneralMathExpression(text, animationController);
+        String result = _inputService.processGeneralMathExpression(text);
+        showResult(result, animationController);
     }
   }
   
-  void _processGeneralMathExpression(String text, AnimationController animationController) {
-    String mathExpression = SpeechText.convertToMathExpression(text);
-    String result = SpeechText.evaluateMathExpression(mathExpression);
-    showResult(result, animationController);
-  }
-
   void handleCalculationSubmission(String text, AnimationController animationController) {
-    try {
-      int userAnswer = int.parse(text);
-      _checkCalculationAnswer(userAnswer, animationController);
-    } catch (e) {
+    int? userAnswer = _inputService.parseCalculationInput(text);
+    
+    if (userAnswer != null) {
+      String result = _problemService.checkCalculationAnswer(userAnswer);
+      showResult(result, animationController);
+    } else {
       showResult(AppStrings.invalidNumberInput, animationController);
     }
   }
   
-  void _checkCalculationAnswer(int userAnswer, AnimationController? animationController) {
-    int correctAnswer = calculateCorrectAnswer(generatedProblem);
-    String resultMessage;
-    
-    if (userAnswer == correctAnswer) {
-      resultMessage = StringFormatter.format(
-        AppStrings.correctCalculationAnswer, 
-        [generatedProblem, correctAnswer]
-      );
-    } else {
-      resultMessage = StringFormatter.format(
-        AppStrings.incorrectCalculationAnswer, 
-        [generatedProblem, correctAnswer]
-      );
-    }
-    
-    showResult(resultMessage, animationController);
-    _resetProblemType();
-  }
-  
-  void _resetProblemType() {
-    setState(() {
-      lastButtonClicked = ProblemType.none;
-    });
-  }
-
-  void handleTrueOrFalseSubmission(String text, AnimationController animationController) {
-    text = text.toLowerCase().trim();
-    bool? userAnswer;
-
-    if (text == AppStrings.trueValue || text == 'true') {
-      userAnswer = true;
-    } else if (text == AppStrings.falseValue || text == 'false') {
-      userAnswer = false;
-    }
-
-    _processTrueFalseAnswer(userAnswer, animationController);
-  }
-  
-  void _processTrueFalseAnswer(bool? userAnswer, AnimationController? animationController) {
-    if (userAnswer == null) {
-      showResult(AppStrings.invalidTrueFalseInput, animationController);
-      return;
-    }
-    
-    String valueAsText = userAnswer ? AppStrings.trueValue : AppStrings.falseValue;
-    String expectedValueAsText = expectedTruthValue! ? AppStrings.trueValue : AppStrings.falseValue;
-    String resultMessage;
-    
-    if (userAnswer == expectedTruthValue) {
-      resultMessage = StringFormatter.format(
-        AppStrings.correctTrueFalseAnswer, 
-        [generatedProblem, valueAsText]
-      );
-    } else {
-      resultMessage = StringFormatter.format(
-        AppStrings.incorrectTrueFalseAnswer, 
-        [generatedProblem, expectedValueAsText]
-      );
-    }
-    
-    showResult(resultMessage, animationController);
-    _resetProblemType();
-  }
-  
-  void handleCalculationVoiceSubmission(String text) {
-    int? userAnswer = MathExpressionProcessor.extractNumberFromVoiceText(text);
+  void handleCalculationVoiceSubmission(String text, AnimationController? animationController) {
+    int? userAnswer = _inputService.extractNumberFromVoiceText(text);
     
     if (userAnswer != null) {
-      _checkCalculationAnswer(userAnswer, null);
+      String result = _problemService.checkCalculationAnswer(userAnswer);
+      showResult(result, animationController);
     } else {
-      showResult(AppStrings.invalidVoiceNumberInput, null);
+      showResult(AppStrings.invalidVoiceNumberInput, animationController);
     }
   }
-
-  void handleTrueOrFalseVoiceSubmission(String text) {
-    bool? userAnswer = VoiceInputParser.parseTrueFalseResponse(
-      text,
-      additionalTrueKeywords: ['oui', 'yes', 'correct'],
-      additionalFalseKeywords: ['non', 'no', 'incorrect']
-    );
-    
-    _processTrueFalseAnswer(userAnswer, null);
+  
+  void handleTrueOrFalseSubmission(String text, AnimationController animationController) {
+    bool? userAnswer = _inputService.parseTrueFalseInput(text);
+    String result = _problemService.checkTrueFalseAnswer(userAnswer);
+    showResult(result, animationController);
   }
-
+  
+  void handleTrueOrFalseVoiceSubmission(String text, AnimationController? animationController) {
+    bool? userAnswer = _inputService.parseTrueFalseVoiceInput(text);
+    String result = _problemService.checkTrueFalseAnswer(userAnswer);
+    showResult(result, animationController);
+  }
+  
   void showResult(String result, AnimationController? animationController) {
-    final emotionResult = EmotionDetector.detectFromText(result);
+    var emotionResult = _emotionService.processEmotionFromText(result, animationController);
     String newEmotion = emotionResult.emotion;
     bool isCorrect = emotionResult.isCorrect;
 
@@ -269,26 +189,25 @@ class QTController {
   }
   
   void _updateEmotionAndText(String newEmotion, String result, bool isCorrect, AnimationController? animationController) {
-    if (_needsEmotionTransition(newEmotion)) {
-      _startEmotionTransition(newEmotion, result, isCorrect, animationController);
+    if (_shouldAnimateEmotionChange(newEmotion)) {
+      _animateEmotionTransition(newEmotion, result, isCorrect, animationController);
     } else {
-      _updateEmotionDirectly(newEmotion, result, isCorrect);
+      _applyEmotionImmediately(newEmotion, result, isCorrect);
     }
   }
   
-  bool _needsEmotionTransition(String newEmotion) {
-    return newEmotion != currentEmotion && currentEmotion != "neutral";
+  bool _shouldAnimateEmotionChange(String newEmotion) {
+    return currentEmotion != "neutral" && newEmotion != currentEmotion;
   }
   
-  void _startEmotionTransition(String newEmotion, String result, bool isCorrect, AnimationController? animationController) {
-    setState(() {
-      previousEmotion = currentEmotion;
-      currentEmotion = newEmotion;
-      isTransitioning = true;
-      resultText = result;
-      isCorrectAnswer = isCorrect;
-      isDefaultBubbleStyle = false;
-    });
+  void _animateEmotionTransition(String newEmotion, String result, bool isCorrect, AnimationController? animationController) {
+    _updateEmotionState(
+      newEmotion: newEmotion,
+      result: result, 
+      isCorrect: isCorrect,
+      isTransitioning: true,
+      storePrevious: true
+    );
 
     if (animationController != null) {
       animationController.reset();
@@ -296,9 +215,29 @@ class QTController {
     }
   }
   
-  void _updateEmotionDirectly(String newEmotion, String result, bool isCorrect) {
+  void _applyEmotionImmediately(String newEmotion, String result, bool isCorrect) {
+    _updateEmotionState(
+      newEmotion: newEmotion,
+      result: result,
+      isCorrect: isCorrect,
+      isTransitioning: false,
+      storePrevious: false
+    );
+  }
+  
+  void _updateEmotionState({
+    required String newEmotion,
+    required String result,
+    required bool isCorrect,
+    required bool isTransitioning,
+    required bool storePrevious,
+  }) {
     setState(() {
+      if (storePrevious) {
+        previousEmotion = currentEmotion;
+      }
       currentEmotion = newEmotion;
+      this.isTransitioning = isTransitioning;
       resultText = result;
       isCorrectAnswer = isCorrect;
       isDefaultBubbleStyle = false;
